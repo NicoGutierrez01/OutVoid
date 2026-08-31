775dcd41ff4b5031abdc1370f2a49d2bb65cbe95
Nico Gutierrez <nicolasgutierrez4245@gmail.com>
2026-08-31 20:54:01 -0300

    nueva forma de juntar power-ups

---
+++ Assets/Scripts/Items/New Power-Ups/PowerUpsChest.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public enum RarezaPowerUp { Comun, Rara, Epica, Legendaria }
+public enum StatModificado { VidaMaxima, EscudoMaximo, DanoArma, VelocidadRecarga, BalasDeFuego }
+
+[CreateAssetMenu(fileName = "NuevoPowerUp", menuName = "Out-Void/Power Up de Cofre")]
+public class PowerUpsChest : ScriptableObject
+{
+    [Header("Información para la UI")]
+    public string nombrePowerUp;
+    
+    [TextArea(2, 3)] 
+    public string descripcion;
+    
+    [Tooltip("El gráfico 2D que aparecerá en el botón de elección")]
+    public Sprite iconoUI;
+    
+    public RarezaPowerUp rareza;
+
+    [Header("Efecto Mecánico")]
+    public StatModificado statAMejorar;
+    
+    [Tooltip("Cuánto suma o multiplica este stat al elegirlo")]
+    public float valorSuma;
+}
